95c3e932ec48365c1af893ecdd87bb65135e14c3
AndyRoush <andyclan@gmail.com>
2018-04-30 12:20:27 -0700

    finished friends.js and added module.exports

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [{
+    name:"Megaman",
+    photo:"https://vignette.wikia.nocookie.net/megaman/images/a/a6/MM10-MegaMan.png/revision/latest?cb=20100720141331",
+    scores:[
+        '6',
+        '3',
+        '5',
+        '4',
+        '4',
+        '2',
+        '1',
+        '1',
+        '8',
+        '7'
+      ]
+  },
+  {
+	name: "Roll",
+	photo: "https://vignette.wikia.nocookie.net/megaman/images/8/8e/TvCRoll.jpg/revision/latest?cb=20090621140309",
+	scores: [
+		'6',
+		'3',
+		'1',
+		'4',
+		'1',
+		'2',
+		'3',
+		'1',
+		'3',
+		'7'
+	]
+}
+]
+  module.exports = friends
